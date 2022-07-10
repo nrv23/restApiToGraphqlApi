e9a2030728a0ec9700d67122829f09064920a1da
nrv23 <navemen23@hotmail.com>
2022-07-10 00:02:18 -0600

    agregar funciones auxiliares para validar datos de entrada

--- src/data/data-races.ts
+++ src/data/data-races.ts
@@ -1,3 +1,4 @@
+import { checkYear } from "../lib/utils";
 import { F1 } from "./data-source";
 
 export class RacesData extends F1{
@@ -8,14 +9,8 @@ export class RacesData extends F1{
 
     async getRacesByYear(year: string) {
 
-        const currentYear = new Date().getFullYear();
+        year = checkYear(year);
         
-        if(isNaN(+year) || +year < 1950 || +year > currentYear) { // isNaN(+year) convierte el valor a numero y compara con isNan si el valor
-            // no es numérico
-            year = currentYear.toString();
-           // return Promise.reject("EL año introducido no es válido");
-        }
-
         return await (await this.get(`${year}.json`,{
             cacheOptions: { ttl:60 } // cachear la consulra durante 60 minutos
         }));
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,25 @@ export function getWikipediaMobileUrl(url: string) {
 
     return !url? '': url.replace('wikipedia','m.wikipedia');
 }
+
+export function checkYear(year: string ) {
+
+    const currentYear = new Date().getFullYear();
+    if(isNaN(+year) || +year < 1950 || +year > currentYear) { // isNaN(+year) convierte el valor a numero y compara con isNan si el valor
+        // no es numérico
+        year = currentYear.toString();
+       // return Promise.reject("EL año introducido no es válido");
+    }
+
+    return year;
+}
+
+export function roundCheck(round: number) {
+
+    if(round >= 100) {
+        return 1
+    } 
+
+    return round;
+
+}
